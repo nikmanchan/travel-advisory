89e4d0aedf4a6509eebb0bf3eeb6c9e6ea09e40d
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-10 22:12:02 -0600

    import SearchBar in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import SearchBar from "./SearchBar/SearchBar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Travel Advisory</h1>
+        <SearchBar />
       </div>
     );
   }
