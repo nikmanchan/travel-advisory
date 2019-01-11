31809621ef591b46ebe98ec0e951eeb907c2233e
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-10 22:40:01 -0600

    change onSubmit to onClick for submit button

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -6,14 +6,24 @@ class SearchBar extends Component {
         country: ''
     }
 
+    handleChange = (property) => (event) => {
+        this.setState({
+            [property]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.country);
+    }
 
   render() {
 
     return (
-        
+
     <div>
-        <input placeholder="Country" value={this.state.country}></input>
-        <button>Submit</button>
+            <input placeholder="country" value={this.state.country} onChange={this.handleChange('country')} ></input>
+            <button onClick={this.handleSubmit}>Submit</button>
     </div>
       
     );
