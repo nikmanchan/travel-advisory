974a4daf52b7d069a7f5fcc4f9cac14434e6c053
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-14 14:27:58 -0600

    add advisory state and API GET request to tugo travel advisory

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,36 +1,59 @@
-import React, { Component } from 'react';
-import './SearchBar.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./SearchBar.css";
+import axios from "axios";
+// import curl from 'curl';
+
+// let config = {
+//     headers: {"X-Auth-API-Key" : "jwjs2jxp2bxuh9m37bd8npp5"}
+// };
 
 class SearchBar extends Component {
-    state = {
-        country: ''
-    }
+  state = {
+    country: "",
+    advisoryState: ""
+  };
 
-    handleChange = (property) => (event) => {
+  handleChange = property => event => {
+    this.setState({
+      [property]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.country);
+
+    // curl -i -H 'Content-Type: application/x-www-form-urlencoded'  'https://api.tugo.com/v1/travelsafe/countries/GR'  -H "X-Auth-API-Key:abcdefghijklmnopqrstuvwxy"
+
+    axios
+      .get(
+        `https://api.tugo.com/v1/travelsafe/countries/${this.state.country}`,
+        {
+          headers: {
+              "X-Auth-API-Key": "pzxtdae4ap3rd4sswp6uhdk2", 'Access-Control-Allow-Origin': '*'
+            }
+        }).then(response => 
         this.setState({
-            [property]: event.target.value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state.country);
-    }
+            advisoryState: response.data.advisories.description
+        }),
+        console.log(this.advisoryState)
+    );
+  };
 
   render() {
-
     return (
-
-    <div>
-            <input placeholder="country" value={this.state.country} onChange={this.handleChange('country')} ></input>
-            <button onClick={this.handleSubmit}>Submit</button>
-    </div>
-      
+      <div>
+        <input
+          placeholder="country"
+          value={this.state.country}
+          onChange={this.handleChange("country")}
+        />
+        <button onClick={this.handleSubmit}>Submit</button>
+        <p>Country: {this.state.country}</p>
+        <p>Advisory: {this.state.advisoryState}</p>
+      </div>
     );
   }
 }
 
 export default SearchBar;
-
-Applicationtravel-advisory Key: pzxtdae4ap3rd4sswp6uhdk2
