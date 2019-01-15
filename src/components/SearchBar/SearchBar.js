ebf9336be3f187d72cb32d570ae6ebd5e4abbb0d
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-14 18:54:20 -0600

    clean up index.html, add mat-ui country select

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 import "./SearchBar.css";
 import axios from "axios";
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
+import { connect } from "react-redux";
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 // jss styles
 const styles = theme => ({
   container: {
-      display: 'flex',
-      flexWrap: 'wrap',
+
+  },
+  root: {
+
   },
   formControl: {
-      margin: theme.spacing.unit,
+    margin: theme.spacing.unit,
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2
   },
   button: {
-      margin: theme.spacing.unit,
-  },
-
+    paddingTop: "4vmin"
+  }
 });
 
 class SearchBar extends Component {
@@ -32,46 +41,61 @@ class SearchBar extends Component {
     this.setState({
       [property]: event.target.value
     });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log(this.state.country);
-
     axios
       .get(
-        `https://api.tugo.com/v1/travelsafe/countries/${this.state.country}`,
+        `https://api.tugo.com/v1/travelsafe/countries/${event.target.value}`,
         {
           headers: {
-              "X-Auth-API-Key": "pzxtdae4ap3rd4sswp6uhdk2", 'Access-Control-Allow-Origin': '*'
-            }
-        }).then(response => 
-            // console.log(response.data)
-            this.props.dispatch({
-              type: 'CHOOSE_COUNTRY', payload: response.data
-            })
-          
-        // this.setState({
-        //     advisoryState: response.data.advisories.description,
-        //     climateInfo: response.data.climateInfo,
-        // }),
-        // console.log(this.advisoryState)
-    );
+            "X-Auth-API-Key": "pzxtdae4ap3rd4sswp6uhdk2",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      )
+      .then(response =>
+        this.props.dispatch({
+          type: "CHOOSE_COUNTRY",
+          payload: response.data
+        })
+      );
   };
 
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-            <input
-            placeholder="country"
-            value={this.state.country}
-            onChange={this.handleChange("country")}
-            />
-            <Button onClick={this.handleSubmit}>Submit</Button>
+        <form className={classes.root} autoComplete="off">
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="age-simple">Countries</InputLabel>
+            <Select
+              value={this.state.country}
+              onChange={this.handleChange("country")}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="ID">Indonesia</MenuItem>
+              <MenuItem value="TH">Thailand</MenuItem>
+              <MenuItem value="PH">Phillippines</MenuItem>
+              <MenuItem value="IN">India</MenuItem>
+              <MenuItem value="JP">Japan</MenuItem>
+              <MenuItem value="HK">Hong Kong</MenuItem>
+              <MenuItem value="AU">Australia</MenuItem>
+              <MenuItem value="NZ">New Zealand</MenuItem>
+              <MenuItem value="GB">United Kingdom</MenuItem>
+              <MenuItem value="IE">Ireland</MenuItem>
+              <MenuItem value="IS">Iceland</MenuItem>
+              <MenuItem value="PT">Portugal</MenuItem>
+              <MenuItem value="ES">Spain</MenuItem>
+              <MenuItem value="GR">Greece</MenuItem>
+              <MenuItem value="IT">Italy</MenuItem>
+              <MenuItem value="DE">Germany</MenuItem>
+              {/* <MenuItem value='CA'>Canada</MenuItem> */}
+              <MenuItem value="MX">Mexico</MenuItem>
+            </Select>
+            <FormHelperText>Choose a country</FormHelperText>
+          </FormControl>
         </form>
-        <p>Country: {this.state.country}</p>
-        <p>Advisory: {this.state.advisoryState}</p>
       </div>
     );
   }
@@ -83,7 +107,7 @@ const mapStateToProps = state => ({
 
 // needed for jss styles
 SearchBar.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(connect(mapStateToProps)(SearchBar));
