7ae4e853c975fbefa0d6e85f4f977784a61d6e63
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-16 14:33:13 -0600

    remove axios import from TravelContent component

--- src/components/TravelContent/TravelContent.js
+++ src/components/TravelContent/TravelContent.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./TravelContent.css";
-// import axios from "axios";
 import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
