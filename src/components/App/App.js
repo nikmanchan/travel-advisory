42f1bd82f5fb747315131acae32eea589dfbeeeb
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-16 10:33:43 -0600

    add styling to app header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
-import {connect} from 'react-redux';
 import CountrySelect from "../CountrySelect/CountrySelect";
 import "./App.css";
 import 'typeface-roboto';
+import PropTypes from 'prop-types';
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+};
 
 class App extends Component {
 
 
   render() {
+    const { classes } = this.props;
+
+
     return (
       <div>
-        <h1>Travel Advisory</h1>
+        <Typography component="h4" variant="h4" gutterBottom className="App-Header">
+          Travel Advisory
+        </Typography>
+        {/* Country Select Form */}
         <CountrySelect />
-        {/* <TravelContent /> */}
       </div>
     );
   }
 }
 
-export default connect()(App);
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(App);
